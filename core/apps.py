7f23b6b891fd5689f1a5f3e411851fa21c30e08d
=MurungaOwen <=owenhood80@gmail.com>
2023-09-29 01:13:34 +0300

    working on core

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HouseappConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'houseapp'
+    name = 'core'
