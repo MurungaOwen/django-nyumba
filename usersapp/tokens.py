a1d6d83f53fe2aaf6e44e15d94dd9c16da98c507
=MurungaOwen <=owenhood80@gmail.com>
2023-10-03 09:27:37 +0300

    email verifi

--- usersapp/tokens.py
+++ usersapp/tokens.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-#import six
+import six
 
 class GenerateEmailToken(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         pass
-        # return (
-        #     six.text_type(user.pk)+six.text_type(timestamp)+six.text_type(user.is_active)
-        # )
+        return (
+        	six.text_type(user.pk)+six.text_type(timestamp)+six.text_type(user.is_active)
+        )
 
 email_token_generator=GenerateEmailToken()
